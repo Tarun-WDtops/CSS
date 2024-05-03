8212e531c3a1974631486cb51835cf215f22d00d
Tarun-WDtops <tjparmar786@gmail.com>
2024-05-04 02:05:33 +0530

    added

---
+++ Practice/Side_bar/index.js
@@ -0,0 +1,6 @@
+const array = ["shankar", "tarun", "rahul"];
+console.log("Array length", array.length);
+for (let i = 0; i < array.length; i++) {
+  const name = array[i];
+  console.log("Name--->", i, name);
+}
